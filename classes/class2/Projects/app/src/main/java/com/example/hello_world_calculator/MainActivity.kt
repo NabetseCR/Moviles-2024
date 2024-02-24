9df336ab09f3984d49f0fa250510f71eecf9e5a2
Esteban Chinchilla <chinchilla.estebandev@gmail.com>
2024-02-23 19:30:51 -0600

    Message added to onStart

--- classes/class2/Projects/app/src/main/java/com/example/hello_world_calculator/MainActivity.kt
+++ classes/class2/Projects/app/src/main/java/com/example/hello_world_calculator/MainActivity.kt
@@ -2,13 +2,17 @@ package com.example.hello_world_calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-
+    override fun onStart() {
+        super.onStart();
+        Toast.makeText(this, "OnStart", Toast.LENGTH_SHORT).show();
+    }
     // logic goes here
 
 }
