8c6861c6e3a0e5933d7c90d2ad042b733e5a5659
Esteban Chinchilla <chinchilla.estebandev@gmail.com>
2024-02-26 22:42:51 -0600

    UI first step done

--- classes/class2/Projects/app/src/main/java/com/example/hello_world_calculator/MainActivity.kt
+++ classes/class2/Projects/app/src/main/java/com/example/hello_world_calculator/MainActivity.kt
@@ -8,6 +8,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val materia1: Int = 7;
+        val materia2: Int = 7;
+        val materia3: Int = 7;
+        val promedio: Int = (materia1 + materia2 + materia3) / 3;
+        if(promedio >= 6){
+            Toast.makeText(this,"Aprobado", Toast.LENGTH_LONG).show();
+        }else if(promedio <= 6){
+            Toast.makeText(this,"Reprobado", Toast.LENGTH_LONG).show();
+        }
     }
     override fun onStart() {
         super.onStart();
