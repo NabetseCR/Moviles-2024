6c1a216438b09e672807165249523c2375991a02
Esteban Chinchilla <chinchilla.estebandev@gmail.com>
2024-03-01 10:16:37 -0600

    calculator validations done

--- classes/class2/Projects/app/src/main/java/com/example/hello_world_calculator/MainActivity.kt
+++ classes/class2/Projects/app/src/main/java/com/example/hello_world_calculator/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.hello_world_calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.widget.*
 
 class MainActivity : AppCompatActivity() {
@@ -28,8 +30,71 @@ class MainActivity : AppCompatActivity() {
         resta = findViewById(R.id.radio_resta)
         div = findViewById(R.id.radio_dividir)
         mult = findViewById(R.id.radio_multiplicar)
+        boton = findViewById(R.id.suma)
 
+        //Adding listeners
+        operando1.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                validateField(operando1)
+                return@setOnEditorActionListener false
+            }
+            false
+        }
+        operando2.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                validateField2(operando2)
+                return@setOnEditorActionListener false
+            }
+            false
+        }
     }
+
+    private fun validateField(operando: EditText) {
+        val name = operando.text.toString().trim()
+        if (TextUtils.isEmpty(name)) {
+            boton.isEnabled = false
+            boton.isClickable = false
+            operando.error = "Valor es requerido"
+        } else {
+            boton.isEnabled = true
+            boton.isClickable = true
+            // Name is not empty, perform further actions
+        }
+    }
+
+    private fun validateField2(operando: EditText) {
+        val name = operando.text.toString().trim()
+        if (TextUtils.isEmpty(name)) {
+            boton.isEnabled = false
+            boton.isClickable = false
+            operando.error = "Valor es requerido"
+        } else if(name.equals("0") && div.isChecked){
+            boton.isEnabled = false
+            boton.isClickable = false
+            operando.error = "Division entre 0 no es valida"
+        } else {
+            boton.isEnabled = true
+            boton.isClickable = true
+            // Name is not empty, perform further actions
+        }
+    }
+    /*fun setFocusChangeListener(editText: EditText){
+        editText.setOnFocusChangeListener { v, hasFocus ->
+
+        }
+        editText.setOnFocusChangeListener{ v, hasFocus ->
+            if(!hasFocus){
+                if(!noVacio(editText)){
+                    editText.error = "Debe indicar un valor"
+                }
+            }
+        }
+    }
+
+     fun noVacio(text: EditText): Boolean {
+        return text.text.toString().isEmpty()
+    }*/
+
     override fun onStart() {
         super.onStart();
         Toast.makeText(this, "OnStart", Toast.LENGTH_SHORT).show();
